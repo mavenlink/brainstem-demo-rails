17b0de167125b137e860aafbbd6f758cda900cc1
Andrew Cantino <andrew@mavenlink.com>
2013-04-28 15:03:07 -0700

    doc

--- lib/api/v1/widget_presenter.rb
+++ lib/api/v1/widget_presenter.rb
@@ -3,29 +3,36 @@ module Api
     class WidgetPresenter < Brainstem::Presenter
       presents "Widget"
 
+      # Available sort orders to expose through the API
       sort_order :updated_at, "widgets.updated_at"
       sort_order :created_at, "widgets.created_at"
 
+      # Default sort order to apply
       default_sort_order "updated_at:desc"
 
+      # Optional filter that applies a lambda.
       filter :location_name do |scope, location_name|
         scope.joins(:locations).where("locations.name = ?", location_name)
       end
 
+      # Optional filter that applies a lambda.
       filter :location_id do |scope, location_id|
         scope.where(:location_id => location_id)
       end
 
+      # Filter with an overridable default that runs on all requests.
       filter :include_legacy_widgets, :default => false do |scope, bool|
         bool ? scope : scope.without_legacy_widgets
       end
 
+      # Return a ruby hash that can be converted to JSON
       def present(widget)
         {
             :name           => widget.name,
             :legacy         => widget.legacy?,
             :updated_at     => widget.updated_at,
             :created_at     => widget.created_at,
+            # Associations can be included by request
             :features       => association(:features),
             :location       => association(:location)
         }
